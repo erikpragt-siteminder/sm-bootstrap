7e0023924238c11b32e5a9ebcb1b0806e5108a6c
Erik Pragt <erik.pragt@siteminder.com>
2016-04-01 12:11:17 +1100

    Added setup script

---
+++ setup-boxen.sh
@@ -0,0 +1,12 @@
+sudo mkdir -p /opt/boxen
+sudo chown ${USER}:staff /opt/boxen
+git clone git@github.com:siteminder-au/boxen.git /opt/boxen/repo
+cd /opt/boxen
+./script/boxen 
+
+echo "Adding env to .bash_profile"
+echo '[ -f /opt/boxen/env.sh ] && source /opt/boxen/env.sh' >> ~/.bash_profile
+
+echo "Boxen has now been setup"
+echo "run: 'boxen siteminder' to install your machine"
+
